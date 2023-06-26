32d9e9d8b699385b6c260451b8d2e3d568e3e4d2
josephcarpinelli <carpinelli.dev@protonmail.ch>
2023-06-26 11:27:49 -0600

    Adds a helper function section with a 'drawShape' function to help simplify the graphics during development. This function was learned about and partially copied from the MDN link provided in the comments above the function.

--- main.js
+++ main.js
@@ -94,6 +94,54 @@ function showGameOverScreen()
 
 
 // ========================================================================= //
+// ======================= Helper Functions ================================ //
+// ========================================================================= //
+/*
+ * ## [Summary](https://developer.mozilla.org/en-US/blog/javascript-shape-drawing-function/?utm_medium=email&utm_source=devnewsletter&utm_campaign=firefox-drumbeat&utm_content=mayhacksnewsletter-global#summary)
+
+This was a little introduction to the `<canvas>` element for drawing on a web page and a few of the methods you can use to draw shapes. If you want to dive deeper into how all the pieces work, here's a recap of what we used:
+
+- [`<canvas>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/canvas), the element on which we can display graphics
+- [`CanvasRenderingContext2D`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D) to draw 2D shapes to the canvas
+- [`translate()`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate) to move the origin to a new position
+- [`lineTo()`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo) to draw a line from one point to another
+- [`closePath()`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/closePath) to join the first point to the last point
+- [`stroke()`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke) to stroke the path with a stroke style
+
+To calculate the position of each point, we used a little bit of maths and trigonometry:
+
+- [`Math.cos()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/cos) to calculate the x position of a point
+- [`Math.sin()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sin) to calculate the y position of a point
+- [`Math.PI`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI) to calculate the angle of rotation in radians
+*/
+function drawShape(x, y, r, sides)
+{
+    // move the canvas to the center position
+    ctx.translate(x, y);
+    for (let i = 0; i < sides; i++)
+    {
+        // calculate the rotation
+        const rotation = ((Math.PI * 2) / sides) * i;
+        // for the first point move to
+        if (i === 0)
+        {
+            ctx.moveTo(r * Math.cos(rotation), r * Math.sin(rotation));
+        }
+        else
+        {
+            // for the rest draw a line
+            ctx.lineTo(r * Math.cos(rotation), r * Math.sin(rotation));
+        }
+    }
+    // close path and stroke it
+    ctx.closePath();
+    ctx.stroke();
+    // reset the translate position
+    ctx.resetTransform();
+}
+
+
+// ========================================================================= //
 // ============================= Main ====================================== //
 // ========================================================================= //
 function main()
