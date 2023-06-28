a590a53d86b34c31f2cd82276b521a1a8cdf9e67
josephcarpinelli <carpinelli.dev@protonmail.ch>
2023-06-27 18:06:07 -0600

    'modules/shapes.js': Adds new functions 'getSideCount(shapeName)' and 'getRadianRotation(shapeName)' which can take a shape string and return the number of sides and the angle of rotation (in radians) that sits the shape on a side after drawing, respectively. Adds new function 'fillShape(color, context)' that will fill a drawn shape with a specified color. This functionality is moved from 'drawShape(pixelRadius, sideCount, rotation, context)'. Finding the center of the canvas is new calculated instead of passed to 'drawShape(...)'. These new functions are used by another new function, 'draw(color, shapeName, pixelRadius, context)' which calls all the appropriate methods to draw, rotate, and fill a shape. These new functions are added to exports.'

--- modules/shapes.js
+++ modules/shapes.js
@@ -16,39 +16,154 @@ To calculate the position of each point, we used a little bit of maths and trigo
 - [`Math.sin()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sin) to calculate the y position of a point
 - [`Math.PI`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI) to calculate the angle of rotation in radians
 */
-const draw = function(xCoordinate, yCoordinate,
-                           radius, sides, color,
-                           context)
+
+const getSideCount = function(shapeName)
 {
+    let sideCount = 0;
+    switch (shapeName.toLowerCase())
+    {
+        case "triangle":
+            sideCount = 3;
+            break;
+        case "square":
+            sideCount = 4;
+            break;
+        case "pentagon":
+            sideCount = 5;
+            break;
+        case "hexagon":
+            sideCount = 6;
+            break;
+        case "septagon":
+        case "heptagon":
+            sideCount = 7;
+            break;
+        case "octagon":
+            sideCount = 8;
+            break;
+        case "nonagon":
+        case "enneagon":
+            sideCount = 9;
+            break;
+        case "decagon":
+            sideCount = 10;
+            break;
+        case "undecagon":
+        case "hendecagon":
+            sideCount = 11;
+            break;
+        case "dodecagon":
+            sideCount = 12;
+            break;
+        default:
+            console.log(`Error! ${shapeName} is not a valid shape.`);
+            break;
+    }
+    return sideCount;
+}
+
+const getRadianRotation = function(shapeName)
+{
+    let degrees = 0;
+    switch (shapeName.toLowerCase())
+    {
+        case "triangle":
+            degrees = 30;
+            break;
+        case "square":
+            degrees = 45;
+            break;
+        case "pentagon":
+            degrees = 54;
+            break;
+        case "hexagon":
+            degrees = 0;
+            break;
+        case "septagon":
+        case "heptagon":
+            degrees = 11;
+            break;
+        case "octagon":
+            degrees = 22;
+            break;
+        case "nonagon":
+        case "enneagon":
+            degrees = 30;
+            break;
+        case "decagon":
+            degrees = 0;
+            break;
+        case "undecagon":
+        case "hendecagon":
+            degrees = 9;
+            break;
+        case "dodecagon":
+            degrees = 15;
+            break;
+        default:
+            console.log(`Error! ${shapeName} is not a valid shape.`);
+            break;
+    }
+    return degrees * (Math.PI / 180);
+}
+
+const drawShape = function(pixelRadius, sideCount, rotation, context)
+{
+    // Set canvas width and height.
+    context.canvas.width = 256;
+    context.canvas.height = 256;
+    // Get center point.
+    const centerX = context.canvas.width / 2;
+    const centerY = context.canvas.height / 2;   
+
     // move the canvas to the center position
-    context.translate(xCoordinate, yCoordinate);
-    for (let sideIndex = 0; sideIndex < sides; sideIndex++)
+    context.translate(centerX, centerY);
+    // Rotate shape.
+    context.rotate(rotation);
+    for (let sideIndex = 0; sideIndex < sideCount; sideIndex++)
     {
         // calculate the rotation
-        const rotation = ((Math.PI * 2) / sides) * sideIndex;
+        const rotation = ((Math.PI * 2) / sideCount) * sideIndex;
         // for the first point move to
         if (sideIndex === 0)
         {
-            context.moveTo(radius * Math.cos(rotation),
-                       radius * Math.sin(rotation));
+            context.moveTo(pixelRadius * Math.cos(rotation),
+                           pixelRadius * Math.sin(rotation));
         }
         else
         {
-            // for the rest dradiusaw a line
-            context.lineTo(radius * Math.cos(rotation),
-                       radius * Math.sin(rotation));
+            // for the rest radiusaw a line
+            context.lineTo(pixelRadius * Math.cos(rotation),
+                           pixelRadius * Math.sin(rotation));
         }
     }
     // close path and stroke it
     context.closePath(); 
     context.stroke();
-    context.fillStyle = color;
-    context.fill()
     // reset the translate position
     context.resetTransform();
 }
 
+const fillShape = function(color, context)
+{
+    context.fillStyle = color;
+    context.fill()
+}
+
+const draw = function(color, shapeName, pixelRadius, context)
+{
+    const sideCount = getSideCount(shapeName);
+    const rotation = getRadianRotation(shapeName);
+    drawShape(pixelRadius, sideCount, rotation, context);
+    fillShape(color, context);
+    
+    return undefined;
+}
+
 export
 {
+    getSideCount,
+    drawShape,
+    fillShape,
     draw,
 };
