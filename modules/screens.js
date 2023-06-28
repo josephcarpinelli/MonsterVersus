e84d75d87d880258f38221902af1e30fb86804a3
josephcarpinelli <carpinelli.dev@protonmail.ch>
2023-06-27 18:10:47 -0600

    'modules/screens.js': Moves comment to a more appropriate spot. Removes canvas calculations which were added to 'modules/shapes.js' in another commit. Updates calls to match previous commit's changes. Adds a second sprite and drawing. Adds completion and game over screen calls to empty if/else statement.

--- modules/screens.js
+++ modules/screens.js
@@ -2,6 +2,11 @@
 import * as Monsters from "./Monsters.js";
 import * as shapes from "./shapes.js";
 
+/*
+// If enemy is KO'd, show game complete screen
+// If player is still alive, show game over screen.
+*/
+
 
 const showTitleScreen = function()
 {
@@ -19,22 +24,24 @@ const showStatsReviewScreen = function()
     console.log("showTitleScreen()");
 }
 
+
 const showBattleScreen = function()
 {
     console.log("showBattleScreen()");
-    const canvas = document.getElementById("player-sprite");
-    canvas.width = 320;  // ~20rem.
-    canvas.width = 320;  // ~20rem.
-    const context = canvas.getContext("2d");
-    const centerX = 160;
-    const centerY = 160;
-    shapes.draw(centerX, centerY, 50, 4, "red", context);
+    const playerContext = document.getElementById("player-sprite").getContext("2d");
+    const opponentContext = document.getElementById("opponent-sprite").getContext("2d");
+    shapes.draw("red", "triangle", 50, opponentContext);
+    shapes.draw("blue", "hexagon", 50, playerContext);
     const playerMonster = showSelectionScreen();
     const opposingMonster = Monsters.MonsterGenerator.createRandomMonster();
     if (playerMonster.battle(opposingMonster))
-    {}
+    {
+        showCompletionScreen();
+    }
     else
-    {}
+    {
+        showGameOverScreen();
+    }
 }
 
 const showCompletionScreen = function(winningMonster)
