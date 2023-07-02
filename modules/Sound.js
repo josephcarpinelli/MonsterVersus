780e8b3d9bd1b8d40f3d732e6075ba0d2cb2eb42
josephcarpinelli <carpinelli.dev@protonmail.ch>
2023-07-02 05:45:46 -0600

    Adds new title screen and hit sounds. Replaces previous sound which is likely under copyright protection.

--- modules/Sound.js
+++ modules/Sound.js
@@ -2,11 +2,12 @@ class Sound
 {
     constructor()
     {
-        this.titleScreenBackground = new Audio("sounds/title-screen.mp3");
-        this.characterSelectionBackground = new Audio("sounds/character-selection.mp3");
-        this.battleBackground = new Audio("sounds/battle.mp3");
-        this.completionBackground = new Audio("sounds/completion-screen.mp3");
-        this.gameOver = new Audio("sounds/game-over.mp3");
+        this.titleScreen = new Audio("sounds/power-on.wav");
+        // this.characterSelectionBackground = new Audio("sounds/character-selection.mp3");
+        // this.battleBackground = new Audio("sounds/battle.mp3");
+        this.hit = new Audio("sounds/hit.mp3");
+        // this.completionBackground = new Audio("sounds/completion-screen.mp3");
+        // this.gameOver = new Audio("sounds/game-over.mp3");
     }
 }
 
